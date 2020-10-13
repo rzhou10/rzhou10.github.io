8fffd2a7b93ebd9fb9b22b33d79982055bd3acaa
rzhou10 <rzhou10@gmail.com>
2020-10-12 22:26:06 -0400

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,9 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'blackmartentrace.com' > CNAME
 
+git init
 git add -A
 git commit -m 'Fixed link to pdf'
 
